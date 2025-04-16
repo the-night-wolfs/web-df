7c10f5a66de1d86178c4058726d10390d51d4634
Harsh Mishra <isecure008.org@gmail.com>
2025-04-16 14:31:46 +0530

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'build' // 👈 this moves build files from dist → build
+    outDir: 'dist' // 👈 this moves build files from dist → build
   }
 })
 
